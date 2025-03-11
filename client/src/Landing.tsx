eda42362e9a15c5a205c395ccc4df006c1e4f77f
PuggyPotato <qingyaurou@gmail.com>
2025-03-11 21:36:49 +0800

    Added the top left box of the Landing Page

--- client/src/Landing.tsx
+++ client/src/Landing.tsx
@@ -8,8 +8,12 @@ function Landing(){
 
     return(
         <>
-            <div id="">
-                <h1>Welcome to Chart Makers!</h1>
+            <div id="landingDiv" className="">
+                <div id="topLeftBox">
+                    <h1 className="" id="landingHeader">Chart Makers</h1>
+                    <p id="landingDescription">Chart Makers provides powerful and easy-to-use tools for creating visually stunning charts and graphs. Whether you need bar charts, pie charts, or interactive data visualizations, our platform helps you present complex information in a clear and engaging way.</p>
+                    <button id="tryNowButton">Try it Now!</button>
+                </div>
             </div>
         </>
     )
